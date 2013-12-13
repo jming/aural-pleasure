0d7100de26502fd4af83304722e3083d4905863f
Ryan Lee <ynh@ynh.com>
2013-12-13 02:45:56 -0500

    derp

--- main.py
+++ main.py
@@ -7,3 +7,8 @@ app = Flask(__name__)
 def main():
 	return render_template('index.html')
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static', 'images'),
+                               'favicon.ico', mimetype='image/png')
+
